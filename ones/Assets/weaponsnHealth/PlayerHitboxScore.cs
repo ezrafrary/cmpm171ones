cf4717e7249c9d395ca4eed7a6fcd53a7d4798e6
ezrafrary <91634826+ezrafrary@users.noreply.github.com>
2025-03-08 13:16:56 -0800

    reorginazation of files and fixed some glaring ui issues with optionsmenu

---
+++ ones/Assets/weaponsnHealth/PlayerHitboxScore.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitboxScore : MonoBehaviour
+{
+
+
+    //this lets us change the hitbox for when a player can gain score, all we need is a reference to the parent player. everything else is taken care of in the scorezone script
+    public GameObject ParentPlayer;
+    
+}
