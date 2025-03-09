616658f1debf871ded0a1f29ecec30e42df63f61
chouske <132114322+chouske@users.noreply.github.com>
2025-03-09 13:46:01 -0700

    Tutorial updates and file size reductions

---
+++ ones/Assets/targetmove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class targetmove : MonoBehaviour
+{
+    float movespeed = 0.01f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            transform.position = new Vector3((transform.position.x + movespeed), transform.position.y, transform.position.z);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        movespeed = movespeed * -1;
+        Debug.Log("entereda");
+    }
+}
